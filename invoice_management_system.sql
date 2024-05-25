6e215cdca276d5d4f08eb58f1a7a52c79b6d081d
Pritom Paul <147104356+PritomPaul-24@users.noreply.github.com>
2024-05-25 20:30:46 +0600

    Add files via upload

---
+++ invoice_management_system.sql
@@ -0,0 +1,116 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2021 at 08:25 PM
+-- Server version: 5.6.21
+-- PHP Version: 5.6.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `invoice_management_system`
+--
+CREATE DATABASE IF NOT EXISTS invoice_management_system DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE invoice_management_system;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order`
+--
+
+CREATE TABLE `invoice_order` (
+  `user_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `order_receiver_name` varchar(255) NOT NULL,
+  `order_receiver_address` varchar(255) NOT NULL,
+  `order_total_before_tax` varchar(255) NOT NULL,
+  `order_total_tax` varchar(255) NOT NULL,
+  `order_tax_per` varchar(255) NOT NULL,
+  `order_total_after_tax` varchar(255) NOT NULL,
+  `order_amount_paid` varchar(255) NOT NULL,
+  `order_total_amount_due` varchar(255) NOT NULL,
+  `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `note` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order_item`
+--
+
+CREATE TABLE `invoice_order_item` (
+  `order_id` int(11) NOT NULL,
+  `item_code` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `order_item_quantity` varchar(255) NOT NULL,
+  `order_item_price` varchar(255) NOT NULL,
+  `order_item_final_amount` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_user`
+--
+
+CREATE TABLE `invoice_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `mobile` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `invoice_user`
+--
+
+INSERT INTO `invoice_user` (`id`, `email`, `password`, `first_name`, `last_name`, `address`, `mobile`) VALUES
+(1, 'pritom@gmail.com', 'pritom12345', 'Pritom ', 'Paul', 'Sylhet', '0171234567');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Indexes for table `invoice_user`
+--
+ALTER TABLE `invoice_user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `invoice_user`
+--
+ALTER TABLE `invoice_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
